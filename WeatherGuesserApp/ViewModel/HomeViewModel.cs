03d76ac17ff89b0028cf8f63cc830c524fd551ed
Mihai <kazepworld@gmail.com>
2018-12-28 17:30:11 +0100

    Setup home viewmodel

--- WeatherGuesserApp/ViewModel/HomeViewModel.cs
+++ WeatherGuesserApp/ViewModel/HomeViewModel.cs
@@ -2,13 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 using WeatherGuesser.Enums;
 using WeatherGuesser.Interfaces;
 using WeatherGuesserApp.Properties;
-using RelayCommand = GalaSoft.MvvmLight.Command.RelayCommand;
+using System.Drawing;
+using RelayCommand = GalaSoft.MvvmLight.CommandWpf.RelayCommand;
 
 namespace WeatherGuesserApp.ViewModel
 {
@@ -17,11 +19,22 @@ namespace WeatherGuesserApp.ViewModel
 
 		private readonly ILearningService _learningService;
 		private readonly INormalizeService _normalizeService;
+		private readonly Dictionary<TrainingItemViewModel, string> _trainingItems;
 
 		public HomeViewModel(ILearningService learningService, INormalizeService normalizeService)
 		{
 			_learningService = learningService;
 			_normalizeService = normalizeService;
+			_trainingItems = new Dictionary<TrainingItemViewModel, string>();
+
+			StatusText = "Idling";
+		}
+
+		private bool _isSvmReady;
+		public bool IsSvmReady
+		{
+			get => _isSvmReady;
+			set => Set(ref _isSvmReady, value);
 		}
 
 		private int _modelCount;
@@ -38,6 +51,13 @@ namespace WeatherGuesserApp.ViewModel
 			set => Set(ref _isTraining, value);
 		}
 
+		private bool _isTrained;
+		public bool IsTrained
+		{
+			get => _isTrained;
+			set => Set(ref _isTrained, value);
+		}
+
 		private string _displayedEvaluationImage;
 		public string DisplayedEvaluationImage
 		{
@@ -45,7 +65,15 @@ namespace WeatherGuesserApp.ViewModel
 			set => Set(ref _displayedEvaluationImage, value);
 		}
 
-		public ObservableCollection<KeyValuePair<string, string>> DataPaths { get; set; } = new ObservableCollection<KeyValuePair<string, string>>();
+		private string _statusText;
+		public string StatusText
+		{
+			get => _statusText;
+			set => Set(ref _statusText, value);
+		}
+
+		public ObservableCollection<TrainingItemViewModel> DataPaths { get; set; } =
+			new ObservableCollection<TrainingItemViewModel>();
 
 		private RelayCommand _openTrainingDataBrowserCommand;
 		public RelayCommand OpenTrainingDataBrowserCommand
@@ -61,20 +89,30 @@ namespace WeatherGuesserApp.ViewModel
 					};
 					browserDialog.ShowDialog();
 
-					if(string.IsNullOrEmpty(browserDialog.SelectedPath))
+					if (string.IsNullOrEmpty(browserDialog.SelectedPath))
 						return;
 
 					var weatherNames = Enum.GetNames(typeof(WeatherType)).Select(n => n.ToLower());
 
 					DataPaths.Clear();
+					_trainingItems.Clear();
 					foreach (var weatherName in weatherNames)
 					{
 						var filesPath = Path.Combine(browserDialog.SelectedPath, weatherName);
 						if (Directory.Exists(filesPath))
 						{
-							foreach (var filePath in Directory.GetFiles(filesPath))
+							var files = Directory.GetFiles(filesPath);
+
+							foreach (var filePath in files)
 							{
-								DataPaths.Add(new KeyValuePair<string, string>(weatherName, Path.GetFileName(filePath)));
+								var trainingItem = new TrainingItemViewModel
+								{
+									WeatherType = (WeatherType)Enum.Parse(typeof(WeatherType), weatherName, true),
+									Name = Path.GetFileName(filePath),
+									IsChecked = false
+								};
+								_trainingItems.Add(trainingItem, filePath);
+								DataPaths.Add(trainingItem);
 							}
 						}
 						else
@@ -85,7 +123,9 @@ namespace WeatherGuesserApp.ViewModel
 						}
 					}
 
-					ModelCount = DataPaths.Count;
+					ModelCount = _trainingItems.Count;
+
+					StatusText = Resources.ReadyToTrain;
 				}, () => !IsTraining));
 			}
 		}
@@ -96,18 +136,25 @@ namespace WeatherGuesserApp.ViewModel
 			get
 			{
 				return _selectEvaluationImageCommand ??
-				       (_selectEvaluationImageCommand = new RelayCommand(() =>
-				       {
-					       var fileBrowser = new OpenFileDialog
-					       {
-						       Multiselect = false,
+					   (_selectEvaluationImageCommand = new RelayCommand(() =>
+					   {
+						   var fileBrowser = new OpenFileDialog
+						   {
+							   Multiselect = false,
 							   CheckFileExists = true,
 							   CheckPathExists = true,
-					       };
-					       fileBrowser.ShowDialog();
+						   };
+						   fileBrowser.ShowDialog();
+						   var filePath = fileBrowser.FileName;
 
-					       DisplayedEvaluationImage = fileBrowser.FileName;
-				       }));
+						   if (File.Exists(filePath))
+						   {
+							   DisplayedEvaluationImage = filePath;
+							   var result = _learningService.GetResult(filePath);
+
+							   StatusText = $"Answer: {result.ToString()}";
+						   }
+					   }, () => IsSvmReady));
 			}
 		}
 
@@ -117,10 +164,92 @@ namespace WeatherGuesserApp.ViewModel
 			get
 			{
 				return _startTrainingCommand ??
-				       (_startTrainingCommand = new RelayCommand(() =>
-				       {
+					   (_startTrainingCommand = new RelayCommand(() =>
+					   {
 						   IsTraining = true;
+						   var worker = new BackgroundWorker()
+						   {
+							   WorkerReportsProgress = false
+						   };
+						   worker.DoWork += (sender, args) =>
+						   {
+							   StatusText = Resources.NormalizingImages;
+
+							   var normalizedImages = new List<KeyValuePair<WeatherType, double[]>>();
+							   foreach (var (itemViewModel, dataPath) in _trainingItems.Select(x => (x.Key, x.Value)))
+							   {
+								   var image = Image.FromFile(dataPath);
+								   var normalizedImage = _normalizeService.GetImage(image);
+								   normalizedImages.Add(new KeyValuePair<WeatherType, double[]>(itemViewModel.WeatherType, normalizedImage));
+
+								   itemViewModel.IsChecked = true;
+							   }
+
+							   StatusText = Resources.Training;
+							   _learningService.Learn(normalizedImages);
+
+							   IsSvmReady = true;
+							   IsTraining = false;
+							   IsTrained = true;
+
+							   StatusText = Resources.TrainingComplete;
+						   };
+						   worker.RunWorkerCompleted += (sender, args) => IsTraining = false;
+						   worker.RunWorkerAsync();
 					   }, () => ModelCount > 0 && !IsTraining));
+			}
+		}
+
+		private RelayCommand _loadMachineCommand;
+		public RelayCommand LoadMachineCommand
+		{
+			get
+			{
+				return _loadMachineCommand ?? (_loadMachineCommand = new RelayCommand(() =>
+						   {
+							   var browserDialog = new OpenFileDialog()
+							   {
+								   Multiselect = false,
+								   Filter = "Data files|*.data",
+								   AddExtension = true
+							   };
+
+							   if (browserDialog.ShowDialog() == DialogResult.OK)
+							   {
+								   var selectedFile = browserDialog.FileName;
+								   if (File.Exists(selectedFile))
+								   {
+									   _learningService.Load(selectedFile);
+									   StatusText = Resources.LoadSuccessfull;
+
+									   IsSvmReady = true;
+									   IsTrained = true;
+								   }
+							   }
+						   }, () => !IsTraining));
+			}
+		}
+
+		private RelayCommand _saveMachineCommand;
+		public RelayCommand SaveMachineCommand
+		{
+			get
+			{
+				return _saveMachineCommand ?? (_saveMachineCommand = new RelayCommand(() =>
+				{
+					var browserDialog = new SaveFileDialog()
+					{
+						AddExtension = true,
+						CheckPathExists = true,
+						FileName = "machine.data",
+						DefaultExt = ".data",
+					};
+
+					if (browserDialog.ShowDialog() == DialogResult.OK)
+					{
+						_learningService.Save(browserDialog.FileName);
+					}
+				}, () => !IsTraining && IsTrained && IsSvmReady));
 			}
 		}
 	}
---
+++ WeatherGuesserApp/ViewModel/TrainingItemViewModel.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using WeatherGuesser.Enums;
+
+namespace WeatherGuesserApp.ViewModel
+{
+	public class TrainingItemViewModel : ViewModelBase
+	{
+		private WeatherType _weatherType;
+		private string _name;
+		private bool _isChecked;
+
+		public WeatherType WeatherType
+		{
+			get => _weatherType;
+			set => Set(ref _weatherType, value);
+		}
+
+		public string Name
+		{
+			get => _name;
+			set => Set(ref _name, value);
+		}
+
+		public bool IsChecked
+		{
+			get => _isChecked;
+			set => Set(ref _isChecked, value);
+		}
+	}
+}
